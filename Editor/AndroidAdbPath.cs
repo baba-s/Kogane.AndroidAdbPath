a3530a1c3b62331b09af6755e3136e7778a6c459
baba-s <shotababa1206@gmail.com>
2023-01-10 20:10:49 +0900

    add

---
+++ Editor/AndroidAdbPath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane
+{
+    public static class AndroidAdbPath
+    {
+        public static string Path =>
+            Application.platform == RuntimePlatform.WindowsEditor
+                ? $"{EditorApplication.applicationPath}/../Data/PlaybackEngines/AndroidPlayer/SDK/platform-tools/adb"
+                : $"{EditorApplication.applicationPath}/../PlaybackEngines/AndroidPlayer/SDK/platform-tools/adb";
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Kogane.AndroidAdbPath
+# Kogane Android Adb Path
+
+Android Build Support のモジュールに含まれる adb のパスを取得できるクラス
+
+## 使用例
+
+```cs
+Debug.Log( AndroidAdbPath.Path );
+```
